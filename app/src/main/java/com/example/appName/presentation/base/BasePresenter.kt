745330d79e10fd6c77fdfd1ef4519ad6b82cb6aa
Jakub Komorowski <jakub.komorowski@codequest.com>
2020-03-06 12:28:30 +0100

    Removed unnecessary Disposable interface from BasePresenter parents

--- app/src/main/java/com/example/appName/presentation/base/BasePresenter.kt
+++ app/src/main/java/com/example/appName/presentation/base/BasePresenter.kt
@@ -7,7 +7,7 @@ import java.io.Serializable
 
 abstract class BasePresenter<VIEW_STATE : Serializable, PARTIAL_VIEW_STATE>(
         private val initialState: VIEW_STATE
-) : Disposable {
+) {
     var currentViewState: VIEW_STATE? = null
         private set
 
@@ -16,13 +16,7 @@ abstract class BasePresenter<VIEW_STATE : Serializable, PARTIAL_VIEW_STATE>(
             currentViewState = it
         }
     }
-
-    private val disposables: CompositeDisposable = CompositeDisposable()
-
-    override fun dispose() = disposables.dispose()
-
-    override fun isDisposed(): Boolean = disposables.isDisposed
-
+    
     private fun subscribeToViewIntents(initialState: VIEW_STATE, vararg observables: Observable<PARTIAL_VIEW_STATE>) = Observable
             .merge(observables.asList())
             .scan(initialState, this::reduceViewState)
