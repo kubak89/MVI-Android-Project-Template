bbc205fe101b87d1bd918973e192b1c6e242a1ea
Bartosz Raczkowski <braczkow@gmail.com>
2021-01-20 14:56:04 +0100

    plop based MVI template solution

---
+++ plopfile.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = function (plop) {
+
+	plop.addHelper('currentDir', function () {
+		return path.normalize(process.env["INIT_CWD"] || process.cwd());
+	});
+
+	plop.addHelper('basePackage', function() {
+		const cwd = path.normalize(process.env["INIT_CWD"] || process.cwd());
+		const substringStart = cwd.indexOf('java/') + 5
+		const package = cwd.substring(substringStart).replaceAll('/', '.')
+
+		return package;
+	})
+
+	plop.setGenerator('screen', {
+		description: 'this is a skeleton plopfile',
+		prompts: [{
+			type: 'input',
+			name: 'name',
+			message: 'Screen name:'
+		}],
+		actions: [
+			{
+				type: "addMany",
+				destination: '{{currentDir}}/{{camelCase name}}',
+				base: `plop-templates/mvi`,
+				templateFiles: `plop-templates/mvi/*.hbs`
+			  }
+		]
+	});
+};
